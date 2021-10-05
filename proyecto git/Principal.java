6b938badb34c1bda2d30bc758b5d69366b47f21c
Pablopingun <pablopingun@gmail.com>
2021-10-05 11:36:52 +0200

    creada la web inicial

---
+++ proyecto git/Principal.java
@@ -0,0 +1,13 @@
+/**
+ * Principal
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        int contador = 1;
+        while(contador <=1000){
+            System.out.println(contador);
+            contador = contador + 1; 
+        }
+    }
+}
